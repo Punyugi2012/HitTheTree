e5066ecb0f033f55f3f4e1deecf0f33889b96350
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-15 14:53:51 +0700

    update

--- HitTheTree/GameViewController.swift
+++ HitTheTree/GameViewController.swift
@@ -10,13 +10,63 @@ import UIKit
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    
+    var mainScene: SCNScene?
+    var mainView: SCNView?
+    var ballNode: SCNNode?
+    var cameraStickNode: SCNNode?
+    var motion = MotionHelper()
+    var motionForce = SCNVector3(0, 0, 0)
+    var sounds: [String:SCNAudioSource] = [:]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let scnView = view as? SCNView {
-            let mainScene = SCNScene(named: "art.scnassets/mainScene.scn")
-            scnView.allowsCameraControl = true
-            scnView.scene = mainScene
+        setupNodes()
+        setupScene()
+        setupSounds()
+    }
+    
+    private func setupScene() {
+        mainView = view as? SCNView
+        mainScene = SCNScene(named: "art.scnassets/mainScene.scn")
+        mainView?.scene = mainScene
+        mainView?.allowsCameraControl = true
+    }
+    
+    private func setupNodes() {
+        ballNode = mainScene?.rootNode.childNode(withName: "ball", recursively: true)
+        cameraStickNode = mainScene?.rootNode.childNode(withName: "cameraStick", recursively: true)
+    }
+    
+    private func setupSounds() {
+
+        if
+            let sawSound = SCNAudioSource(fileNamed: "chainsaw.wav"),
+            let jumpSound = SCNAudioSource(fileNamed: "jump.wav") {
+            sawSound.load()
+            jumpSound.load()
+            sawSound.volume = 0.3
+            jumpSound.volume = 0.4
+            sounds["saw"] = sawSound
+            sounds["jump"] = jumpSound
+        }
+        if let backgroundSound = SCNAudioSource(fileNamed: "background.mp3") {
+            backgroundSound.volume = 0.3
+            backgroundSound.loops = true
+            backgroundSound.load()
+            let backgroundPlayer = SCNAudioPlayer(source: backgroundSound)
+            ballNode?.addAudioPlayer(backgroundPlayer)
+        }
+    }
+    
+    private func sceneViewTapped(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: mainView)
+        let hitResults = mainView?.hitTest(location, options: nil)
+        if let results = hitResults, let node = results.first?.node, node.name == "ball" {
+            let jumpSound = sounds["jump"]!
+            ballNode?.runAction(SCNAction.playAudio(jumpSound, waitForCompletion: false))
+            ballNode?.physicsBody?.applyForce(SCNVector3(0, 5, -2), asImpulse: true)
         }
     }
     
@@ -27,5 +77,5 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
 }
