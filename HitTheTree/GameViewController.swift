cc1a52ba24717c5f2517fd5524870817e76275e8
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-15 16:10:15 +0700

    finished

--- HitTheTree/GameViewController.swift
+++ HitTheTree/GameViewController.swift
@@ -30,8 +30,9 @@ class GameViewController: UIViewController {
     private func setupScene() {
         mainView = view as? SCNView
         mainScene = SCNScene(named: "art.scnassets/mainScene.scn")
+        mainScene?.physicsWorld.contactDelegate = self
         mainView?.scene = mainScene
-        mainView?.allowsCameraControl = true
+        mainView?.delegate = self
         
         let tapRecog = UITapGestureRecognizer(target: self, action: #selector(sceneViewTapped(recognizer:)))
         tapRecog.numberOfTapsRequired = 1
@@ -41,11 +42,12 @@ class GameViewController: UIViewController {
     
     private func setupNodes() {
         ballNode = mainScene?.rootNode.childNode(withName: "ball", recursively: true)
+        ballNode?.physicsBody?.contactTestBitMask = 2
         cameraStickNode = mainScene?.rootNode.childNode(withName: "cameraStick", recursively: true)
     }
     
     private func setupSounds() {
-
+        
         if
             let sawSound = SCNAudioSource(fileNamed: "chainsaw.wav"),
             let jumpSound = SCNAudioSource(fileNamed: "jump.wav") {
@@ -71,7 +73,7 @@ class GameViewController: UIViewController {
         if let results = hitResults, let node = results.first?.node, node.name == "ball" {
             let jumpSound = sounds["jump"]!
             ballNode?.runAction(SCNAction.playAudio(jumpSound, waitForCompletion: false))
-            ballNode?.physicsBody?.applyForce(SCNVector3(0, 4, -2), asImpulse: true)
+            ballNode?.physicsBody?.applyForce(SCNVector3(0, 3, -2), asImpulse: true)
         }
     }
     
@@ -83,4 +85,42 @@ class GameViewController: UIViewController {
         return true
     }
     
+}
+
+extension GameViewController: SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        let ball = ballNode?.presentation
+        let ballPosition = ball?.position
+        if let ballPosition = ballPosition {
+            let targetPosition = SCNVector3(ballPosition.x, ballPosition.y + 5, ballPosition.z + 5)
+            let cameraPosition = cameraStickNode?.position
+            let camDamping:Float = 0.3
+            if let cameraPosition = cameraPosition {
+                let xComponent = cameraPosition.x * (1 - camDamping) + targetPosition.x * camDamping
+                let yComponent = cameraPosition.y * (1 - camDamping) + targetPosition.y * camDamping
+                let zComponent = cameraPosition.z * (1 - camDamping) + targetPosition.z * camDamping
+                cameraStickNode?.position = SCNVector3(xComponent, yComponent, zComponent)
+            }
+        }
+        motion.getAccelerometerData { (x, y, z) in
+            self.motionForce = SCNVector3(x * 0.05, 0, (y + 0.8) * -0.05 )
+        }
+        ballNode?.physicsBody?.velocity += motionForce
+    }
+}
+
+extension GameViewController: SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        if contact.nodeA.name == "tree" && contact.nodeB.name == "ball" {
+            contact.nodeA.isHidden = true
+            let saw = sounds["saw"]!
+            ballNode?.runAction(SCNAction.playAudio(saw, waitForCompletion: false))
+            let wait = SCNAction.wait(duration: 15)
+            let unHideAction = SCNAction.run { (node) in
+                node.isHidden = false
+            }
+            let actionSequence = SCNAction.sequence([wait, unHideAction])
+            contact.nodeA.runAction(actionSequence)
+        }
+    }
 }
